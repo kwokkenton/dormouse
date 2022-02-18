d7758199f71080eb18a2941ed3149d6329da0af2
Kenton Kwok <54623370+kwokkenton@users.noreply.github.com>
2022-02-18 18:28:59 +0000

    Add focusing option
    Also add switch cases

--- simulations/twod_proj.m
+++ simulations/twod_proj.m
@@ -7,11 +7,16 @@
 % Field Patterns Example.
 %
 % author: Kenton Kwok
-% date: 7/2/2022
+% date: 7/2/2022, last updated 18/2/2022
 
 
 clearvars;
 
+% =========================================================================
+% SET SIMULATION CASE
+% =========================================================================
+
+beam_type = 'focus_wrap';
 % =========================================================================
 % SIMULATION
 % =========================================================================
@@ -26,8 +31,9 @@ kgrid = kWaveGrid(Nx, dx, Ny, dy);
 % define the properties of the propagation medium, with sound speed and 
 % POWER LAW Absorption
 medium.sound_speed = 1500;  % [m/s]
-medium.alpha_coeff = 0.75;  % [dB/(MHz^y cm)]
+medium.alpha_coeff = 0;     % [dB/(MHz^y cm)]
 medium.alpha_power = 1.5;
+medium.density = 1000;      % [kg/m^3]
 
 % create the time array
 kgrid.makeTime(medium.sound_speed);
@@ -42,22 +48,39 @@ source.p_mask(x_offset, start_index:start_index + num_elements - 1) = 1;
 % define the properties of the tone burst used to drive the transducer
 sampling_freq = 1/kgrid.dt;     % [Hz]
 steering_angle = 30;            % [deg]
+x_focus = 0;                    % [m]
+z_focus = 10e-3;                % [m]
 element_spacing = dx;           % [m]
 tone_burst_freq = 1e6;          % [Hz]
-tone_burst_cycles = 8;
+tone_burst_cycles = 5;
 
 % create an element index relative to the centre element of the transducer
 element_index = -(num_elements - 1)/2:(num_elements - 1)/2;
 
-% use geometric beam forming to calculate the tone burst offsets for each
-% transducer element based on the element index
-%tone_burst_offset = 60 + element_spacing * element_index * ...
-    %sin(steering_angle * pi/180) / (medium.sound_speed * kgrid.dt);
+switch beam_type
+    % this modifies the tone_burst offsets, note that they are
+    % element-indexed
+    case 'steer'
+        % use geometric beam forming to calculate the tone burst offsets for 
+        % each transducer element based on the element index
+        tone_burst_offset = 60 + element_spacing * element_index * ...
+            sin(steering_angle * pi/180) / (medium.sound_speed * kgrid.dt);
+    case 'steer_wrap'
+%         apply a phase wrapping, equivalent to modulo operator
+        tone_burst_offset = 60 + mod(element_spacing * element_index * ...
+            sin(steering_angle * pi/180) / (medium.sound_speed* kgrid.dt), ... 
+               1/(tone_burst_freq* kgrid.dt)) ;
+    case 'focus'
+        r = sqrt(z_focus^2 + x_focus^2);
+        tone_burst_offset = 100 + (r - sqrt((x_focus - element_spacing * ... 
+             element_index).^2 + z_focus^2))/(medium.sound_speed * kgrid.dt);
+    case 'focus_wrap'
+        r = sqrt(z_focus^2 + x_focus^2);
+        tone_burst_offset = 100 + mod(r - sqrt((x_focus - element_spacing * ... 
+             element_index).^2 + z_focus^2)/(medium.sound_speed * ...
+             kgrid.dt), 1/(tone_burst_freq* kgrid.dt));
+end 
 
-% apply a phase wrapping, equivalent to modulo operator
-tone_burst_offset = 60 + mod(element_spacing * element_index * ...
-    sin(steering_angle * pi/180) / (medium.sound_speed* kgrid.dt), ... 
-       1/(tone_burst_freq* kgrid.dt)) ;
 
 % create the tone burst signals
 source.p = toneBurst(sampling_freq, tone_burst_freq, tone_burst_cycles, ...
@@ -77,20 +100,22 @@ source.p = toneBurst(sampling_freq, tone_burst_freq, tone_burst_cycles, ...
 
 % define the first line sensor region by specifying the location of
 % opposing corners
-rect1_x_start = 100;
+rect1_x_start = 1;
 rect1_y_start = 1;
-rect1_x_end = 200;
+rect1_x_end = Nx;
 rect1_y_end = Ny;
 
 % assign the list of opposing corners to the sensor mask
 sensor.mask = [rect1_x_start, rect1_y_start, rect1_x_end, rect1_y_end].';
 
-% set the record mode to capture the final wave-field
+% set the record mode to capture a time series of pressure and velocity
 sensor.record = {'p_max'};
+sensor.record = {'p', 'u'};
 
 % assign the input options
 %input_args = { 'PMLInside', false, 'PlotPML', false};
-input_args = {'DisplayMask', source.p_mask};
+input_args = {'DisplayMask', source.p_mask, 'PlotLayout', false};
+
 % run the simulation
 sensor_data = kspaceFirstOrder2D(kgrid, medium, source, sensor, input_args{:});
 
@@ -115,13 +140,33 @@ figure;
 stackedPlot(kgrid.t_array(1:num_source_time_points) * scale, source.p);
 xlabel(['Time [' prefix 's]']);
 ylabel('Input Signals');
+
+%%
+figure;
+plot(element_index, tone_burst_offset*kgrid.dt);
+xlabel('Element Index');
+ylabel('Time delay')
 %%
 % VISUALISATION FOR THE FINAL PATTERN
 % plot the simulated sensor data
-figure;
-mx = max(abs(sensor_data.p_max(:)));
-imagesc(sensor_data.p_max, [-mx, mx]);
-colormap(getColorMap);
-ylabel('Sensor Position');
-xlabel('Time Step');
-colorbar;
+% figure;
+% mx = max(abs(sensor_data.p_max(:)));
+% imagesc(sensor_data.p_max, [-mx, mx]);
+% colormap(getColorMap);
+% ylabel('Sensor Position');
+% xlabel('Time Step');
+% colorbar;
+
+%%
+data = sensor_data.p;
+%save('pressure_unwrapped.mat', 'data');
+
+%%
+test = sqrt((x_focus - element_spacing * ... 
+             element_index).^2 + z_focus^2)/(medium.sound_speed * kgrid.dt);
+             
+test = test - min(test);
+ttt = mod(test, 1/(tone_burst_freq * kgrid.dt));
+plot(ttt);
+%hold on;
+%plot(test);
